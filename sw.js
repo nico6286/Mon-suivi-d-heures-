22c1e936a6746e64ad2ea2b317b3e2bd913653b1
nico6286 <160365328+nico6286@users.noreply.github.com>
2026-04-12 23:27:49 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,7 @@
-const CACHE_NAME = 'suivi-heures-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './app.js',
-  './manifest.json',
-  './1000016253.jpg'
-];
-
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
-  );
+const CACHE_NAME = 'v1';
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(['./', './index.html', './app.js', './manifest.json', './1000016253.jpg'])));
 });
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
-  );
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(res => res || fetch(e.request)));
 });
