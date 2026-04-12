7a03b9d6b2db381dc549a6844be93ab8a171d7f6
nico6286 <160365328+nico6286@users.noreply.github.com>
2026-04-12 21:24:52 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'suivi-pro-v1';
+
+// Installation et activation forcée
+self.addEventListener('install', event => {
+    self.skipWaiting();
+    event.waitUntil(
+        caches.open(CACHE_NAME).then(cache => {
+            return cache.addAll(['./', './index.html', './app.js', './manifest.json']);
+        }).catch(err => console.log("Cache error", err))
+    );
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request).then(response => {
+            return response || fetch(event.request);
+        })
+    );
+});
